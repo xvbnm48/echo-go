de0628b07030f700b3a3b69bec2cefb9315c4bdf
fariz <fariz.wisnu@bluebirdgroup.com>
2023-01-20 11:29:44 +0700

    init

---
+++ server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.GET("/users/:id", getUser)
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func getUser(c echo.Context) error {
+	id := c.Param("id")
+	return c.JSON(http.StatusOK, map[string]string{
+		"code":    "200",
+		"message": "success",
+		"data":    id,
+	})
+}
